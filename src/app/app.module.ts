40028663d516841feb292084a1bc77166dc5ad7b
AxyIX <alexmauster@mail.ru>
2019-07-03 22:03:55 +0700

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupListComponent } from './components/group-list/group-list.component';
+import { MemberListComponent } from './components/member-list/member-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupListComponent,
+    MemberListComponent
   ],
   imports: [
     BrowserModule,
