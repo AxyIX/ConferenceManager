17a8bc492825a5263b83c882c24d1fd982644bb0
AxyIX <alexmauster@mail.ru>
2019-07-03 22:13:04 +0700

    Edit data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {Group} from '../models/Group';
+import {PhoneState} from '../models/PhoneState';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  groups: Group[];
+  constructor() {
+    this.groups = [
+      { id: 0, name: 'group 1', members: [
+          { id: 0, name: 'vasia pupochkin', active: false, phoneState: PhoneState.IDLE },
+          { id: 1, name: 'ivan ivanov', active: true, phoneState: PhoneState.CALL },
+          { id: 3, name: 'pavel ibragimov', active: false, phoneState: PhoneState.IDLE },
+        ] },
+      { id: 1, name: 'group 2', members: [
+          { id: 4, name: 'vasia pupochkin2', active: false, phoneState: PhoneState.IDLE },
+          { id: 5, name: 'ivan ivanov2', active: true, phoneState: PhoneState.CALL },
+          { id: 6, name: 'pavel ibragimov2', active: false, phoneState: PhoneState.IDLE },
+        ] }
+    ];
+  }
 
-  constructor() { }
+  getGroups(): Observable<Group[]> {
+    return of(this.groups);
+  }
 }
