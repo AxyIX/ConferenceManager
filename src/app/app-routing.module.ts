a36cdf45f0a54dacfe8a930a01bba8afcf6e71cf
AxyIX <alexmauster@mail.ru>
2019-07-04 00:01:19 +0700

    Edit group component Edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GroupListComponent} from './components/group-list/group-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'groups', component: GroupListComponent},
+  {path: '', redirectTo: '/groups', pathMatch: 'full'},
+  {path: 'groups/:groupId', component: GroupListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/group-list/group-list.component.ts
+++ src/app/components/group-list/group-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Group} from '../../models/Group';
+import {DataService} from '../../services/data.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-group-list',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-list.component.css']
 })
 export class GroupListComponent implements OnInit {
-
-  constructor() { }
+  groups: Group[];
+  membersCount: number;
+  constructor(private dataService: DataService, private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.dataService.getGroups().subscribe(groups => {
+      this.groups = groups;
+      this.membersCount = 0;
+      this.groups.map(group => {
+        this.membersCount += group.members.length;
+      });
+    });
   }
 
 }
